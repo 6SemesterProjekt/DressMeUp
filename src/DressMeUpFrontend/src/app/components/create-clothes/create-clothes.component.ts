2213808d599327a706ee47a0ddfb87ea4afa7480
mark-knudsen <marksvendstrupknudsen@gmail.com>
2023-05-14 00:40:07 +0200

    design for tabs 3 "Udforsk"

--- src/DressMeUpFrontend/src/app/components/create-clothes/create-clothes.component.ts
+++ src/DressMeUpFrontend/src/app/components/create-clothes/create-clothes.component.ts
@@ -1,39 +1,38 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ModalController, ToastController } from '@ionic/angular';
-import { Clothes } from 'src/app/interfaces/clothes';
-import { ClothesService } from 'src/app/services/clothes.service';
-import { PhotoService, UserPhoto } from 'src/app/services/photo.service';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ModalController, ToastController } from "@ionic/angular";
+import { Clothes } from "src/app/interfaces/clothes";
+import { ClothesService } from "src/app/services/clothes.service";
+import { PhotoService, UserPhoto } from "src/app/services/photo.service";
 
 @Component({
-  selector: 'app-create-clothes',
-  templateUrl: './create-clothes.component.html',
-  styleUrls: ['./create-clothes.component.scss'],
+  selector: "app-create-clothes",
+  templateUrl: "./create-clothes.component.html",
+  styleUrls: ["./create-clothes.component.scss"],
 })
 export class CreateClothesComponent implements OnInit {
   @Output() formSubmit = new EventEmitter();
 
   postForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private modalController: ModalController,
     public toastController: ToastController,
     private clothesService: ClothesService,
-    public photoService: PhotoService) {
-
+    public photoService: PhotoService
+  ) {
     this.postForm = this.formBuilder.group({
-      clothesType: ['', Validators.required],
-      color: ['', Validators.required],
-      fabric: ['', Validators.required],
-      seasons: ['', Validators.required],
-      filterTags: ['', Validators.required],
-      brand: ['', [Validators.required, Validators.minLength(2)]],
-      image: ['', Validators.required],
-      name: ['', Validators.required],
+      clothesType: ["", Validators.required],
+      color: ["", Validators.required],
+      fabric: ["", Validators.required],
+      seasons: ["", Validators.required],
+      filterTags: ["", Validators.required],
+      brand: ["", [Validators.required, Validators.minLength(2)]],
+      image: ["", Validators.required],
+      name: ["", Validators.required],
     });
   }
-
-
 
   /*   "clothesType": 1, 
     "color": "ffffff",
@@ -44,7 +43,7 @@ export class CreateClothesComponent implements OnInit {
     "image": "img.png",
     "name": "sorte sko" */
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   onSubmit() {
     if (this.postForm.valid) {
@@ -59,28 +58,28 @@ export class CreateClothesComponent implements OnInit {
         Name: this.postForm.value.name,
       };
 
-
       this.clothesService.createNewItem(newCloth).subscribe((i) => {
         if (i) {
-          this.modalController.dismiss(this.postForm.value, 'confirm');
+          this.modalController.dismiss(this.postForm.value, "confirm");
           this.presentToast();
           console.log(newCloth);
         }
       });
     }
+  }
 
-
+  onCancel() {
+    this.modalController.dismiss(null, "cancel");
   }
 
   async presentToast() {
     const toast = await this.toastController.create({
-      message: 'Tøj oprettet!',
+      message: "Tøj oprettet!",
       duration: 3000, // milliseconds
-      position: 'top', // 'top', 'middle', or 'bottom'
+      position: "top", // 'top', 'middle', or 'bottom'
     });
     toast.present();
   }
-
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
--- src/DressMeUpFrontend/src/app/tab1/tab1.page.ts
+++ src/DressMeUpFrontend/src/app/tab1/tab1.page.ts
@@ -1,29 +1,30 @@
-import { Component } from '@angular/core';
-import { MyClothesComponent } from '../components/my-clothes-component/my-clothes-component/my-clothes-component.component';
-import { ClothesService } from '../services/clothes.service';
-import { Clothes } from '../interfaces/clothes';
-import { PhotoService } from '../services/photo.service';
-import { CreateClothesComponent } from '../components/create-clothes/create-clothes.component';
-import { ModalController } from '@ionic/angular';
+import { Component } from "@angular/core";
+import { MyClothesComponent } from "../components/my-clothes-component/my-clothes-component/my-clothes-component.component";
+import { ClothesService } from "../services/clothes.service";
+import { Clothes } from "../interfaces/clothes";
+import { PhotoService } from "../services/photo.service";
+import { CreateClothesComponent } from "../components/create-clothes/create-clothes.component";
+import { ModalController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-
   clothes: Clothes[] = [];
 
-  constructor(private modalController: ModalController, private clothesService: ClothesService, public photoService: PhotoService) { }
+  constructor(
+    private modalController: ModalController,
+    private clothesService: ClothesService,
+    public photoService: PhotoService
+  ) {}
 
   async ngOnInit() {
-    console.log('getting data')
+    console.log("getting data");
     this.clothesService
       .getAllclothes()
-      .subscribe(
-        data => this.clothes = data
-      )
+      .subscribe((data) => (this.clothes = data));
   }
 
   addPhotoToGallery() {
@@ -35,13 +36,12 @@ export class Tab1Page {
   }
 
   onClothesCardTapped(clothes: Clothes) {
-    console.log(clothes)
+    console.log(clothes);
   }
-
 
   async openPostForm() {
     const modal = await this.modalController.create({
-      component: CreateClothesComponent
+      component: CreateClothesComponent,
     });
     await modal.present();
     const { data } = await modal.onDidDismiss();
