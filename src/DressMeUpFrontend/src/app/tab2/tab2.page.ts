8ccba5c71faf92ef30f6b2e95d57efca286d3c1e
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2023-05-19 10:21:59 +0200

    It works again.

--- src/DressMeUpFrontend/src/app/tab2/tab2.page.ts
+++ src/DressMeUpFrontend/src/app/tab2/tab2.page.ts
@@ -1,8 +1,6 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { OutfitsService } from '../services/outfits.service';
 import { IOutfit } from '../interfaces/outfit';
-import { OutfitComponent } from '../components/outfit/outfit.component';
-import { Gesture, GestureController, GestureDetail } from '@ionic/angular';
 
 
 @Component({
@@ -10,7 +8,7 @@ import { Gesture, GestureController, GestureDetail } from '@ionic/angular';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements AfterViewInit {
+export class Tab2Page implements OnInit {
 
   outfits: IOutfit[] = [];
 
@@ -20,54 +18,9 @@ export class Tab2Page implements AfterViewInit {
     '../../assets/Images/png/2-jacket.png',
     '../../assets/Images/png/3-jacket.png'];
 
-  constructor(private outfitService: OutfitsService, private gestureCtrl: GestureController) {
+  constructor(private outfitService: OutfitsService) {
 
     // The `true` above ensures that callbacks run inside NgZone.
-  }
-
-
-
-  ngAfterViewInit() {
-    const gesture: Gesture = this.gestureCtrl.create({
-      el: document.querySelector('.my-element'),
-      gestureName: 'swipe',
-      direction: 'x',
-      threshold: 0,
-      gesturePriority: 1,
-      passive: false,
-      canStart: () => true,
-      onStart: () => { },
-      onMove: (ev: GestureDetail) => this.onSwipeMove(ev),
-      onEnd: (ev: GestureDetail) => this.onSwipeEnd(ev)
-    });
-
-    gesture.enable();
-  }
-  onSwipeMove(ev: GestureDetail) {
-    // Implement the logic for handling the swipe gesture while moving
-    if (ev.deltaX > 0) {
-      // Swipe right
-      this.onSwipeRight();
-    } else {
-      // Swipe left
-      this.onSwipeLeft();
-    }
-  }
-
-  onSwipeEnd(ev: GestureDetail) {
-    // Implement any necessary cleanup or additional logic after the swipe gesture ends
-  }
-
-
-  onSwipeRight() {
-    this.currentIndex = (this.currentIndex + 1) % this.imagePaths.length;
-    console.log("currentIndex: " + this.currentIndex);
-    console.log("We make the call for right image! (ideally)");
-  }
-  onSwipeLeft() {
-    this.currentIndex = (this.currentIndex - 1 + this.imagePaths.length) % this.imagePaths.length;
-    console.log("currentIndex: " + this.currentIndex);
-    console.log("We make the call for left image! (ideally)");
   }
 
 
--- src/DressMeUpFrontend/src/config/gesture-config.ts
+++
@@ -1,8 +0,0 @@
-import { HammerGestureConfig } from '@angular/platform-browser';
-import * as Hammer from 'hammerjs';
-
-export class GestureConfig extends HammerGestureConfig {
-    override = {
-        swipe: { direction: Hammer.DIRECTION_HORIZONTAL },
-    };
-}
