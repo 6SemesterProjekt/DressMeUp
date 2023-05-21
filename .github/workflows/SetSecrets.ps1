3d8c32781c81174be9dec7983849a0ca48e88ff5
Niko D <nikodojan@live.de>
2023-05-21 21:24:52 +0200

    script

---
+++ .github/workflows/SetSecrets.ps1
@@ -0,0 +1,26 @@
+﻿param(
+
+    [string]$storageAccountName,
+    [string]$storageSasToken,
+    [string]$apiBaseUrl,
+    [string]$storageConnectionString
+)
+
+cd..
+cd..
+cd src/DressMeUpFrontend/src/environments
+
+$content = @"
+export const environment = {
+  production: true,
+  apiBaseUrl: `"$apiBaseUrl`",
+  azureStorageAccountName: `"$storageAccountName`",
+  storageAccountSasToken: `"$storageSasToken`",
+  storageConnectionString: `"$storageConnectionString`"
+};
+"@
+
+$file = "environment.prod.ts"
+
+
+Set-Content -Path $file -Value $content
