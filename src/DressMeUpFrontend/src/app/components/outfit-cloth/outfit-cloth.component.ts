16ea2f063e4f7cf31dd8985b2bb69e67fbb02eda
mark-knudsen <marksvendstrupknudsen@gmail.com>
2023-05-22 21:23:50 +0200

    Select cloth on generator

--- src/DressMeUpFrontend/src/app/components/outfit-cloth/outfit-cloth.component.ts
+++ src/DressMeUpFrontend/src/app/components/outfit-cloth/outfit-cloth.component.ts
@@ -23,25 +23,32 @@ export class OutfitClothComponent {
   @ViewChild("rightArrow", { static: true, read: ElementRef })
   rightArrow: ElementRef;
 
-  @Input('clickSubject') clickSubject: Subject<any>;
+  @Input("clickSubject") clickSubject: Subject<any>;
 
   @Input() clothes: IClothes[] = [];
   currentImageIndex: number = 0;
   isSwiping: boolean = false;
 
-  constructor(private gestureCtrl: GestureController) { }
+  childComponents: any[] = [
+    { id: 1, image: "child1.jpg", selected: false },
+    { id: 2, image: "child2.jpg", selected: false },
+    { id: 3, image: "child3.jpg", selected: false },
+    // Add more child components as needed
+  ];
+  constructor(private gestureCtrl: GestureController) {}
 
   ngOnInit() {
     const cloth: IClothes = {
-      "Brand": "Nike",
-      "ClothesType": ClothesType.Accessoires,
-      "Seasons": [],
-      "Color": [],
-      "FilterTags": [],
-      "Fabric": [],
-      "Name": "Name",
-      "Image": "https://saphotostest.blob.core.windows.net/photos/photo1684488018710.png"
-    }
+      Brand: "Nike",
+      ClothesType: ClothesType.Accessoires,
+      Seasons: [],
+      Color: [],
+      FilterTags: [],
+      Fabric: [],
+      Name: "Name",
+      Image:
+        "https://saphotostest.blob.core.windows.net/photos/photo1684488018710.png",
+    };
     this.clothes.push(cloth);
     const gesture = this.gestureCtrl.create({
       el: this.image.nativeElement,
@@ -51,7 +58,7 @@ export class OutfitClothComponent {
       },
       onMove: (ev) => {
         // Handle swipe movement if needed
-        if (!this.isSwiping) {
+        if (!this.isSwiping || this.isSelected == true) {
           return;
         }
         const imageElement: HTMLElement = this.image.nativeElement;
@@ -72,19 +79,40 @@ export class OutfitClothComponent {
     });
     gesture.enable(true);
     this.clickSubject.subscribe((e) => {
-      console.log('Parent sends greetings');
+      console.log("Parent sends greetings");
       this.getRandomIndex();
     });
   }
+
+  private pressTimer: ReturnType<typeof setTimeout>;
+  isSelected: boolean = false;
+
+  startPressTimer() {
+    this.pressTimer = setTimeout(() => {
+      this.selectImage();
+    }, 500); // Adjust the duration as needed for your long press threshold
+  }
+
+  clearPressTimer() {
+    clearTimeout(this.pressTimer);
+  }
+
+  selectImage() {
+    this.isSelected = !this.isSelected; // Toggle isSelected flag
+  }
+
   getRandomIndex() {
-    let max = this.clothes.length;
-    this.currentImageIndex = Math.floor(Math.random() * max);
+    if (this.isSelected == false) {
+      let max = this.clothes.length;
+      this.currentImageIndex = Math.floor(Math.random() * max);
+    }
   }
 
   loadPreviousImage() {
-    if (!this.isSwiping) {
+    if (!this.isSwiping && this.isSelected == false) {
       this.currentImageIndex =
-        (this.currentImageIndex - 1 + this.clothes.length) % this.clothes.length;
+        (this.currentImageIndex - 1 + this.clothes.length) %
+        this.clothes.length;
       this.reloadImage();
       console.log("currentIndex: " + this.currentImageIndex);
       console.log("We make the call for left image! (ideally)");
@@ -92,7 +120,7 @@ export class OutfitClothComponent {
   }
 
   loadNextImage() {
-    if (!this.isSwiping) {
+    if (!this.isSwiping && this.isSelected == false) {
       this.currentImageIndex =
         (this.currentImageIndex + 1) % this.clothes.length;
       this.reloadImage();
@@ -100,7 +128,6 @@ export class OutfitClothComponent {
       console.log("currentIndex: " + this.currentImageIndex);
       console.log("We make the call for right image! (ideally)");
     }
-
   }
 
   reloadImage() {
